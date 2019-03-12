f52d33369a876aba57e011689acb8a4939f265dc
Julien <peloton@lal.in2p3.fr>
2019-03-12 08:36:57 +0100

    Add test helper for Spark tests. Add unit tests for classification module

--- python/fink_broker/tester.py
+++ python/fink_broker/tester.py
@@ -32,3 +32,52 @@ def regular_unit_tests(global_args=None, verbose=False):
         np.set_printoptions(legacy="1.13")
 
     doctest.testmod(globs=global_args, verbose=verbose)
+
+def spark_unit_tests(global_args=None, verbose=False):
+    """ Base commands for the regular unit test suite
+
+    Include this routine in the main of a module, and execute:
+    python3 mymodule.py
+    to run the tests.
+
+    It should exit gracefully if no error (exit code 0),
+    otherwise it will print on the screen the failure.
+
+    Parameters
+    ----------
+    global_args: dict, optional
+        Dictionary containing user-defined variables to
+        be passed to the test suite. Default is None.
+    verbose: bool
+        If True, print useful debug messages.
+        Default is False.
+
+    Examples
+    ----------
+    Set "toto" to "myvalue", such that it can be used during tests:
+    >>> globs = globals()
+    >>> globs["toto"] = "myvalue"
+    >>> regular_unit_tests(global_args=globs)
+    """
+    from pyspark.sql import SparkSession
+    from pyspark import SparkConf
+
+    conf = SparkConf()
+    confdic = {"spark.jars.packages": "org.apache.spark:spark-streaming-kafka-0-10-assembly_2.11:2.4.0,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0,org.apache.spark:spark-avro_2.11:2.4.0"}
+    conf.setMaster("local[2]")
+    conf.setAppName("test")
+    for k, v in confdic.items():
+        conf.set(key=k, value=v)
+    spark = SparkSession\
+        .builder\
+        .appName("test")\
+        .config(conf=conf)\
+        .getOrCreate()
+
+    global_args["spark"] = spark
+
+    # Numpy introduced non-backward compatible change from v1.14.
+    if np.__version__ >= "1.14.0":
+        np.set_printoptions(legacy="1.13")
+
+    doctest.testmod(globs=global_args, verbose=verbose)
