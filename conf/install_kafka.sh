27ea08da6481b22540f733fdb492f7aff3ceae0f
Abhishek Chauhan <chauhan.abhishek1729@gmail.com>
2019-06-03 12:55:27 +0530

    add script to install Kafka

---
+++ conf/install_kafka.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Author: Abhishek Chauhan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+KAFKA_VERSION=2.2.0
+
+wget https://www.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_2.12-${KAFKA_VERSION}.tgz
+tar -xvzf kafka_2.12-${KAFKA_VERSION}.tgz
+rm kafka_2.12-${KAFKA_VERSION}.tgz
+
+export KAFKA_HOME=$( pwd )/kafka_2.12-${KAFKA_VERSION}
