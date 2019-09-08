0d7e688e5447a6272cd287bb597d90f32a233f4e
Abhishek Chauhan <chauhan.abhishek1729@gmail.com>
2019-09-08 20:13:30 +0530

    change hbase version to 2.1.6

--- conf/install_hbase.sh
+++ conf/install_hbase.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 set -e
 
-HBASE_VERSION=2.1.5
+HBASE_VERSION=2.1.6
 
 wget http://www-us.apache.org/dist/hbase/${HBASE_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
 tar -zxvf hbase-${HBASE_VERSION}-bin.tar.gz
