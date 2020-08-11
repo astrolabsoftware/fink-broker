a4d933548fb821a84012450aa123e1f016ce1d32
Julien <peloton@lal.in2p3.fr>
2020-08-11 12:19:27 +0200

    Update the asteroid module call with new args

--- fink_broker/science.py
+++ fink_broker/science.py
@@ -137,7 +137,10 @@ def apply_science_modules(df: DataFrame, logger: Logger) -> DataFrame:
 
     # Apply level one processor: asteroids
     logger.info("New processor: asteroids")
-    args_roid = ['cfid', 'cmagpsf', 'candidate.ssdistnr']
+    args_roid = [
+        'cjd', 'cmagpsf',
+        'candidate.ndethist', 'candidate.sgscore1',
+        'candidate.ssdistnr', 'candidate.distpsnr1']
     df = df.withColumn('roid', roid_catcher(*args_roid))
 
     # Drop temp columns
