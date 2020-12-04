0c93b4753043bd4f50689a79544885ff9745c5d8
Julien <peloton@lal.in2p3.fr>
2020-12-04 13:43:32 +0100

    Update the livestream service: new filters, relevant topic names, and trigger time (Closes #408)

--- bin/distribute.py
+++ bin/distribute.py
@@ -34,16 +34,20 @@ from fink_broker.loggingUtils import get_fink_logger, inspect_application
 
 # User-defined topics
 userfilters = [
-    'fink_filters.filter_rrlyr.filter.rrlyr',
-    'fink_filters.filter_snlike.filter.snialike'
+    'fink_filters.filter_early_sn_candidates.filter.early_sn_candidates',
+    'fink_filters.filter_sn_candidates.filter.sn_candidates',
+    'fink_filters.filter_sso_ztf_candidates.filter.sso_ztf_candidates',
+    'fink_filters.filter_sso_fink_candidates.filter.sso_fink_candidates'
 ]
+# does not work because of nested args
+# 'fink_filters.filter_microlensing_candidates.filter.microlensing_candidates',
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     args = getargs(parser)
 
     # Initialise Spark session
-    spark = init_sparksession(name="distribute", shuffle_partitions=2)
+    spark = init_sparksession(name="distribute_{}".format(args.night), shuffle_partitions=2)
 
     # The level here should be controlled by an argument.
     logger = get_fink_logger(spark.sparkContext.appName, args.log_level)
@@ -53,7 +57,10 @@ def main():
 
     # Connect to the TMP science database
     df = connect_to_raw_database(
-        args.scitmpdatapath, args.scitmpdatapath + "/*", latestfirst=False)
+        args.scitmpdatapath + "/year={}/month={}/day={}".format(args.night[0:4], args.night[4:6], args.night[6:8]),
+        args.scitmpdatapath + "/year={}/month={}/day={}".format(args.night[0:4], args.night[4:6], args.night[6:8]),
+        latestfirst=False
+    )
 
     # Drop partitioning columns
     df = df.drop('year').drop('month').drop('day')
@@ -64,13 +71,14 @@ def main():
     cnames[cnames.index('cutoutScience')] = 'struct(cutoutScience.*) as cutoutScience'
     cnames[cnames.index('cutoutTemplate')] = 'struct(cutoutTemplate.*) as cutoutTemplate'
     cnames[cnames.index('cutoutDifference')] = 'struct(cutoutDifference.*) as cutoutDifference'
+    cnames[cnames.index('mulens')] = 'struct(mulens.*) as mulens'
     cnames[cnames.index('prv_candidates')] = 'explode(array(prv_candidates)) as prv_candidates'
     cnames[cnames.index('candidate')] = 'struct(candidate.*) as candidate'
 
     broker_list = args.distribution_servers
     for userfilter in userfilters:
         # The topic name is the filter name
-        topicname = userfilter.split('.')[-1]
+        topicname = 'fink_' + userfilter.split('.')[-1] + '_ztf'
 
         # Apply user-defined filter
         df_tmp = apply_user_defined_filter(df, userfilter)
@@ -90,6 +98,7 @@ def main():
             .option("kafka.sasl.mechanism", "SCRAM-SHA-512")\
             .option("topic", topicname)\
             .option("checkpointLocation", args.checkpointpath_kafka + topicname)\
+            .trigger(processingTime='{} seconds'.format(args.tinterval)) \
             .start()
 
     # Keep the Streaming running until something or someone ends it!
