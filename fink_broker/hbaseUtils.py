42b45ae4fcfd84b46a7b6bf0413ccf823702158e
Julien <peloton@lal.in2p3.fr>
2021-03-24 21:49:45 +0100

    Add knscore in HBase tables

--- fink_broker/hbaseUtils.py
+++ fink_broker/hbaseUtils.py
@@ -44,7 +44,7 @@ def load_science_portal_column_names():
     --------
     >>> cols_i, cols_d, cols_b = load_science_portal_column_names()
     >>> print(len(cols_d))
-    8
+    9
     """
     # Column family i
     cols_i = [
@@ -65,7 +65,8 @@ def load_science_portal_column_names():
         col('mulens.class_1').alias('mulens_class_1'),
         col('mulens.class_2').alias('mulens_class_2'),
         'roid',
-        'nalerthist'
+        'nalerthist',
+        'knscore'
     ]
 
     # Column family binary
