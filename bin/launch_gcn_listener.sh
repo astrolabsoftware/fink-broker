61d26c8fa7df4877da01d9d981c7b564d7c35c22
JulienPeloton <peloton@lal.in2p3.fr>
2022-10-24 15:08:55 +0200

    Abs path

--- bin/launch_gcn_listener.sh
+++ bin/launch_gcn_listener.sh
@@ -3,4 +3,4 @@
 mkdir -p $HOME/fink_grb_out
 
 # should be up 24/7
-nohup fink_grb gcn_stream start --config conf_cluster/fink_grb.conf > $HOME/fink_grb_out/gcn_stream.log &
+nohup fink_grb gcn_stream start --config $FINK_HOME/conf_cluster/fink_grb.conf > $HOME/fink_grb_out/gcn_stream.log &
