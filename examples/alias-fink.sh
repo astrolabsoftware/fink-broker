76c4a552bde53e244ecd6bcda7fd10997c0f4f8c
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-08-14 08:05:26 +0200

    Add aliases

---
+++ examples/alias-fink.sh
@@ -0,0 +1,11 @@
+
+FINK_BROKER_SRC_DIR="$HOME/src/fink-broker"
+FINK_ALERT_SIM_SRC_DIR="$HOME/src/fink-alert-simulator"
+FINKCTL_SRC_DIR="$HOME/src/finkctl"
+
+alias cdfa="cd $FINK_ALERT_SIM_SRC_DIR"
+alias cdfb="cd $FINK_BROKER_SRC_DIR"
+alias cdfc="cd $FINKCTL_SRC_DIR"
+
+# Aliases for fink-broker
+#########################
