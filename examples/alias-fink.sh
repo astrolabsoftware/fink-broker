f1b3ff258880fbf69d5b521a4715f333c668c2a1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-08-14 11:30:29 +0200

    Update current feature

--- examples/alias-fink.sh
+++ examples/alias-fink.sh
@@ -15,3 +15,4 @@ alias cdfb="cd $FINK_BROKER_SRC_DIR"
 alias cdfc="cd $FINKCTL_SRC_DIR"
 
 alias fns="export MINIMAL=true NSCIENCE=true"
+alias fbp="$FINK_BROKER_SRC_DIR/build.sh && $FINK_BROKER_SRC_DIR/push.sh"
