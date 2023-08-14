4b7efffe3409ba097251c16440976223869fa290
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-08-14 08:30:45 +0200

    Update current feature

--- examples/alias-fink.sh
+++ examples/alias-fink.sh
@@ -1,3 +1,10 @@
+# Aliases for Fink
+
+# Source it from your .bashrc or .bash_aliases with:
+# if [ -f $HOME/src/fink-broker/examples/alias-fink.sh ]; then
+#     . $HOME/src/fink-broker/examples/alias-fink.sh
+# fi
+
 
 FINK_BROKER_SRC_DIR="$HOME/src/fink-broker"
 FINK_ALERT_SIM_SRC_DIR="$HOME/src/fink-alert-simulator"
@@ -7,5 +14,4 @@ alias cdfa="cd $FINK_ALERT_SIM_SRC_DIR"
 alias cdfb="cd $FINK_BROKER_SRC_DIR"
 alias cdfc="cd $FINKCTL_SRC_DIR"
 
-# Aliases for fink-broker
-#########################
+alias fns="export MINIMAL=true NSCIENCE=true"
