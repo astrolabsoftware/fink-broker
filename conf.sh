8040a5fa9c5f97b69beed47de88893a022125168
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-13 10:37:28 +0100

    Run ciux-based CI
    - Hack to install go module in v3
    - Increase wait time for topics retrieval in CI
    - Use ciux to install itest deps
      * Use latest finkctl in gha

--- conf.sh
+++ conf.sh
@@ -1,6 +1,5 @@
-# Set variable below to empty string in order to
-# build and install current development version
-FINK_BROKER_RELEASE=''
+
+. "$CIUXCONFIG"
 
 # Do not launch science pipeline if true
 NOSCIENCE="${NOSCIENCE:-false}"
@@ -15,7 +14,7 @@ MINIMAL="${MINIMAL:-false}"
 REPO="gitlab-registry.in2p3.fr/astrolabsoftware/fink"
 CI_REPO="${CI_REPO:-$REPO}"
 # Tag to apply to the built image, or to identify the image to be pushed
-TAG=${FINK_BROKER_RELEASE:-$(git -C $DIR describe --dirty --always)}
+TAG="$FINK_BROKER_VERSION"
 # WARNING "spark-py" is hard-coded in spark build script
 
 # Disable science pipeline
@@ -59,5 +58,3 @@ KAFKA_CLUSTER="kafka-cluster"
 # Default values are the ones set in fink-alert-simulator CI environment
 KAFKA_SOCKET=${KAFKA_SOCKET:-"kafka-cluster-kafka-external-bootstrap.kafka:9094"}
 KAFKA_TOPIC=${KAFKA_TOPIC:-"ztf-stream-sim"}
-
-FINK_ALERT_SIMULATOR_DIR="/tmp/fink-alert-simulator"
