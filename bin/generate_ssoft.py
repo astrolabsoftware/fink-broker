1c173a55a636dc0e112ef1a293b9fe5016528fd9
JulienPeloton <peloton@lal.in2p3.fr>
2023-11-16 14:53:30 +0100

    Missing deps

--- bin/generate_ssoft.py
+++ bin/generate_ssoft.py
@@ -22,6 +22,7 @@ import datetime
 from fink_broker.parser import getargs
 
 from fink_broker.loggingUtils import get_fink_logger, inspect_application
+from fink_broker.sparkUtils import init_sparksession
 
 from fink_spins.ssoft import build_the_ssoft
 
