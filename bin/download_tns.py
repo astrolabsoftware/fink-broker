a67c72d27f6d1fc6fe193a252d36858620c34725
JulienPeloton <peloton@lal.in2p3.fr>
2024-01-05 10:00:22 +0100

    Update the header

--- bin/download_tns.py
+++ bin/download_tns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2023 AstroLab Software
+# Copyright 2023-2024 AstroLab Software
 # Author: Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
