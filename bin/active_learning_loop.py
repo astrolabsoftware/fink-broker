25b138d487f1cb835ff8b44c8b4806b609ba8720
anaismoller <anaismoller@gmail.com>
2024-05-07 15:17:35 +1000

    rank by high probability + model is still hard coded

--- bin/active_learning_loop.py
+++ bin/active_learning_loop.py
@@ -30,7 +30,9 @@ from fink_broker.loggingUtils import get_fink_logger, inspect_application
 
 from fink_filters.classification import extract_fink_classification
 from fink_filters.filter_anomaly_notification.filter_utils import msg_handler_slack
-from fink_filters.filter_anomaly_notification.filter_utils import get_data_permalink_slack
+from fink_filters.filter_anomaly_notification.filter_utils import (
+    get_data_permalink_slack,
+)
 
 from fink_science import __file__
 from fink_science.random_forest_snia.processor import rfscore_sigmoid_full
@@ -48,23 +50,25 @@ def append_slack_messages(slack_data: list, row: dict) -> None:
         Pandas DataFrame row as dictionary. Contains
         Fink data.
     """
-    t1 = f'ID: <https://fink-portal.org/{row.objectId}|{row.objectId}>'
-    t2 = f'''
-EQU: {row.ra},   {row.dec}'''
+    t1 = f"ID: <https://fink-portal.org/{row.objectId}|{row.objectId}>"
+    t2 = f"""
+EQU: {row.ra},   {row.dec}"""
 
-    t3 = f'Score: {round(row.al_snia_vs_nonia, 3)}'
-    t4 = f'Classification: {row.classification}'
+    t3 = f"Score: {round(row.al_snia_vs_nonia, 3)}"
+    t4 = f"Classification: {row.classification}"
     cutout, curve, cutout_perml, curve_perml = get_data_permalink_slack(row.objectId)
     curve.seek(0)
     cutout.seek(0)
     cutout_perml = f"<{cutout_perml}|{' '}>"
     curve_perml = f"<{curve_perml}|{' '}>"
-    slack_data.append(f'''==========================
+    slack_data.append(
+        f"""==========================
 {t1}
 {t2}
 {t3}
 {t4}
-{cutout_perml}{curve_perml}''')
+{cutout_perml}{curve_perml}"""
+    )
 
 
 def main():
@@ -74,8 +78,7 @@ def main():
 
     # Initialise Spark session
     spark = init_sparksession(
-        name="AL_loop_{}".format(args.night),
-        shuffle_partitions=2
+        name="AL_loop_{}".format(args.night), shuffle_partitions=2
     )
 
     # The level here should be controlled by an argument.
@@ -85,75 +88,76 @@ def main():
     inspect_application(logger)
 
     # Connect to the aggregated science database
-    path = '{}/science/year={}/month={}/day={}'.format(
-        args.agg_data_prefix,
-        args.night[:4],
-        args.night[4:6],
-        args.night[6:8]
+    path = "{}/science/year={}/month={}/day={}".format(
+        args.agg_data_prefix, args.night[:4], args.night[4:6], args.night[6:8]
     )
     df = load_parquet_files(path)
 
     # Retrieve time-series information
-    to_expand = [
-        'jd', 'fid', 'magpsf', 'sigmapsf'
-    ]
+    to_expand = ["jd", "fid", "magpsf", "sigmapsf"]
 
     # Append temp columns with historical + current measurements
-    prefix = 'c'
+    prefix = "c"
     for colname in to_expand:
         df = concat_col(df, colname, prefix=prefix)
 
     # Add classification
     cols = [
-        'cdsxmatch', 'roid', 'mulens',
-        'snn_snia_vs_nonia', 'snn_sn_vs_all', 'rf_snia_vs_nonia',
-        'candidate.ndethist', 'candidate.drb', 'candidate.classtar',
-        'candidate.jd', 'candidate.jdstarthist', 'rf_kn_vs_nonkn', 'tracklet'
+        "cdsxmatch",
+        "roid",
+        "mulens",
+        "snn_snia_vs_nonia",
+        "snn_sn_vs_all",
+        "rf_snia_vs_nonia",
+        "candidate.ndethist",
+        "candidate.drb",
+        "candidate.classtar",
+        "candidate.jd",
+        "candidate.jdstarthist",
+        "rf_kn_vs_nonkn",
+        "tracklet",
     ]
-    df = df.withColumn('classification', extract_fink_classification(*cols))
+    df = df.withColumn("classification", extract_fink_classification(*cols))
 
     # model path
     curdir = os.path.dirname(os.path.abspath(__file__))
-    model = curdir + '/data/models/for_al_loop/model_20231009.pkl'
+    model = curdir + "/data/models/for_al_loop/model_20240422.pkl"
 
     # Run SN classification using AL model
-    rfscore_args = ['cjd', 'cfid', 'cmagpsf', 'csigmapsf']
-    rfscore_args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
-    rfscore_args += [F.lit(1), F.lit(3), F.lit('diff')]
+    rfscore_args = ["cjd", "cfid", "cmagpsf", "csigmapsf"]
+    rfscore_args += [F.col("cdsxmatch"), F.col("candidate.ndethist")]
+    rfscore_args += [F.lit(1), F.lit(3), F.lit("diff")]
     rfscore_args += [F.lit(model)]
-    df = df.withColumn(
-        'al_snia_vs_nonia',
-        rfscore_sigmoid_full(*rfscore_args)
-    )
+    df = df.withColumn("al_snia_vs_nonia", rfscore_sigmoid_full(*rfscore_args))
 
     # Distance to the middle of the distribution of scores
-    df = df.withColumn('dist_center', F.abs(df['al_snia_vs_nonia'] - 0.5))
+    df = df.withColumn("dist_center", F.abs(df["al_snia_vs_nonia"] - 0.5))
 
     # Filter
-    c1 = df['cdsxmatch'].isin(return_list_of_eg_host().tolist())
-    c2 = df['candidate.dec'] < 20.
-    c3 = (df['candidate.jd'] - df['candidate.jdstarthist']) <= 20.
-    c4 = df['candidate.drb'] > 0.5
-    c5 = df['candidate.classtar'] > 0.4
-    c6 = df['al_snia_vs_nonia'] > 0.0
+    c1 = df["cdsxmatch"].isin(return_list_of_eg_host().tolist())
+    c2 = df["candidate.dec"] < 20.0
+    c3 = (df["candidate.jd"] - df["candidate.jdstarthist"]) <= 20.0
+    c4 = df["candidate.drb"] > 0.5
+    c5 = df["candidate.classtar"] > 0.4
+    c6 = df["al_snia_vs_nonia"] > 0.0
 
     df_filt = df.filter(c1).filter(c2).filter(c3).filter(c4).filter(c5).filter(c6)
 
     cols_ = [
-        'objectId',
-        'candidate.ra',
-        'candidate.dec',
-        'dist_center',
-        'classification',
-        'al_snia_vs_nonia',
-        'rf_snia_vs_nonia',
-        'candidate.ndethist'
+        "objectId",
+        "candidate.ra",
+        "candidate.dec",
+        "dist_center",
+        "classification",
+        "al_snia_vs_nonia",
+        "rf_snia_vs_nonia",
+        "candidate.ndethist",
     ]
 
     pdf = df_filt.select(cols_).toPandas()
-    pdf = pdf.sort_values('dist_center', ascending=True)
+    pdf = pdf.sort_values("dist_center", ascending=True)
 
-    init_msg = f'Number of candidates for the night {args.night}: {len(pdf)} ({len(np.unique(pdf.objectId))} unique objects).'
+    init_msg = f"Number of candidates for the night {args.night}: {len(pdf)} ({len(np.unique(pdf.objectId))} unique objects)."
 
     slack_data = []
     for _, row in pdf.head(30).iterrows():
@@ -163,8 +167,9 @@ def main():
 
     # Filter for high probabilities
     pdf_hp = pdf[pdf["al_snia_vs_nonia"] > 0.5]
+    pdf_hp = pdf_hp.sort_values("al_snia_vs_nonia", ascending=False)
 
-    init_msg = f'Number of candidates for the night {args.night} (high probability): {len(pdf_hp)} ({len(np.unique(pdf_hp.objectId))} unique objects).'
+    init_msg = f"Number of candidates for the night {args.night} (high probability): {len(pdf_hp)} ({len(np.unique(pdf_hp.objectId))} unique objects)."
 
     slack_data = []
     for _, row in pdf_hp.head(30).iterrows():
