83ad36812710384daac214ae878f1935a04304ca
JulienPeloton <peloton@lal.in2p3.fr>
2024-05-23 08:08:49 +0200

    Fix headers

--- fink_broker/mm2distribute.py
+++ fink_broker/mm2distribute.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Utilities for Fink-MM integration"""
+"""Utilities for multi-messenger activities (distribution)"""
 import os
 import time
 
