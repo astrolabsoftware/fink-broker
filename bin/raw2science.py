04b5bc5bbbbd3b09d555ed3109c5d6a11532cd21
JulienPeloton <peloton@lal.in2p3.fr>
2024-06-11 07:30:37 +0200

    Do not filter alerts containing i-band only in the history

--- bin/raw2science.py
+++ bin/raw2science.py
@@ -161,9 +161,7 @@ def main():
         df = df.filter(df["candidate.nbad"] == 0).filter(df["candidate.rb"] >= 0.55)
 
         # Discard an alert if it is in i band
-        # or if it contains i band measurements in history
         df = df.filter(df["candidate.fid"] != 3)
-        df = df.filter(~F.array_contains(df["prv_candidates.fid"], 3))
 
         # Apply science modules
         _LOG.info("Applying science modules")
