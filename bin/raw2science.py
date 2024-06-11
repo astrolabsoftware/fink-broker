5cf8e91711fb24c77bcf254a36571c338bdea731
JulienPeloton <peloton@lal.in2p3.fr>
2024-06-11 07:27:24 +0200

    Check filtering step

--- bin/raw2science.py
+++ bin/raw2science.py
@@ -84,6 +84,21 @@ def main():
             latestfirst=False,
         )
 
+    # DEBUG STATEMENTS
+    df_static = spark.read.format('parquet').load(
+        rawdatapath + "/year={}/month={}/day={}".format(
+            args.night[0:4],
+            args.night[4:6],
+            args.night[6:8]
+        )
+    )
+
+    print("COUNT BEFORE: {}".format(df_static.count()))
+    df_filtered = df_static\
+        .filter(df_static['candidate.nbad'] == 0)\
+        .filter(df_static['candidate.rb'] >= 0.55)
+    print("COUNT AFTER: {}".format(df_filtered.count()))
+
     # Apply science modules
     if "candidate" in df.columns:
         # Apply quality cuts
