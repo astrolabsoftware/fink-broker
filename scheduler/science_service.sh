410c106ab6abe142d26f18e0bd4e4f031c09ae7c
Julien <peloton@lal.in2p3.fr>
2024-06-13 08:13:12 +0200

    Update ZTF schedule (#857)

--- scheduler/science_service.sh
+++ scheduler/science_service.sh
@@ -16,7 +16,7 @@ while true; do
             echo "Launching service"
 
             # LEASETIME must be computed by taking the difference between now and max end (5pm CE(S)T)
-            LEASETIME=$(( `date +'%s' -d '17:00 today'` - `date +'%s' -d 'now'` ))
+            LEASETIME=$(( `date +'%s' -d '20:00 today'` - `date +'%s' -d 'now'` ))
 
             ${FINK_HOME}/bin/fink start raw2science \
                 -c ${FINK_HOME}/conf_cluster/fink.conf.ztf_raw2science \
