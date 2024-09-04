db477829cbf61405755c4d54db8dc67fe9b59a90
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-09-04 11:34:26 +0200

    WIP Set py4j log level to INFO

--- fink_broker/logging_utils.py
+++ fink_broker/logging_utils.py
@@ -66,6 +66,8 @@ def init_logger(log_level: str = "INFO") -> logging.Logger:
     streamHandler.setFormatter(formatter)
     logger.addHandler(streamHandler)
 
+    logging.getLogger('py4j').setLevel(logging.INFO)
+
     return logger
 
 
