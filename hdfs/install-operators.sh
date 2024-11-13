588f448326578bbfeb195c23def4d905fa2dcfe1
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-11-13 09:35:38 +0100

    Update current feature

--- hdfs/install-operators.sh
+++ hdfs/install-operators.sh
@@ -1,2 +1,3 @@
 curl -L -o /tmp/stackablectl https://github.com/stackabletech/stackable-cockpit/releases/download/stackablectl-24.7.0/stackablectl-x86_64-unknown-linux-gnu
+chmod +x /tmp/stackable
 /tmp/stackablectl operator install commons=24.7.0   secret=24.7.0 listener=24.7.0  zookeeper=24.7.0  hdfs=24.7.0
