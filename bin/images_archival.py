8a2509a86074621adc2a44a821f422522668bb57
JulienPeloton <peloton@lal.in2p3.fr>
2024-11-18 16:10:32 +0100

    Fix missing imports

--- bin/images_archival.py
+++ bin/images_archival.py
@@ -21,7 +21,8 @@ import pyspark.sql.functions as F
 
 from fink_broker.parser import getargs
 from fink_broker.spark_utils import init_sparksession, load_parquet_files
-
+from fink_broker.hbase_utils import assign_column_family_names
+from fink_broker.hbase_utils import push_to_hbase
 
 from fink_broker.logging_utils import get_fink_logger, inspect_application
 
