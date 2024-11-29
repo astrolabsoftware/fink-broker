e77e04b39dca4977f4240e657ee6cb50cb1a17e6
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-11-29 15:03:22 +0100

    Move HDFS install to fink-cd

--- hdfs/install.sh
+++ hdfs/install.sh
@@ -6,13 +6,5 @@ DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 STCK_BIN="/tmp/stackable"
 
-# WIP moving to argocd
-# ink "Install Stackable operators"
-# curl -L -o $STCK_BIN https://github.com/stackabletech/stackable-cockpit/releases/download/stackablectl-24.7.0/stackablectl-x86_64-unknown-linux-gnu
-# chmod +x $STCK_BIN
-# $STCK_BIN operator install commons=24.7.0   secret=24.7.0 listener=24.7.0  zookeeper=24.7.0  hdfs=24.7.0
-
-ink "Install HDFS instance"
-kubectl apply -f $DIR -n default
-
+ink "Create hdfs directory"
 $DIR/mkdir.sh
