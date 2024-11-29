4ed42e9c3e297211333024b61ba80a97cec03325
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-11-29 15:03:22 +0100

    Move HDFS install to fink-cd

--- hdfs/mkdir.sh
+++ hdfs/mkdir.sh
@@ -6,22 +6,24 @@ set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
+NS=hdfs
+
 # Wait for HDFS statefulset to be available
 # TODO improve this
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=hdfs --timeout=300s -n default
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=zookeeper --timeout=300s -n default
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=hdfs --timeout=300s -n $NS
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=zookeeper --timeout=300s -n $NS
 sleep 60
 
 hdfs_url="hdfs://simple-hdfs-namenode-default-0.simple-hdfs-namenode-default.default:8020"
 
 # Check if pod hdfs-client exists
-if ! kubectl get -n default pod hdfs-client &> /dev/null; then
-    kubectl run -n default --image apache/hadoop:3.4.0 hdfs-client -- sleep infinity
+if ! kubectl get -n "$NS" pod hdfs-client &> /dev/null; then
+    kubectl run -n "$NS" --image apache/hadoop:3.4.0 hdfs-client -- sleep infinity
 fi
 
-kubectl wait -n default --for=condition=ready pod/hdfs-client
+kubectl wait -n "$NS" --for=condition=ready pod/hdfs-client
 
-kubectl exec -n default -it hdfs-client -- sh -c "export HADOOP_USER_NAME=stackable && \
+kubectl exec -n "$NS" -it hdfs-client -- sh -c "export HADOOP_USER_NAME=stackable && \
     hdfs dfs -fs $hdfs_url -mkdir -p /user/185 && \
     hdfs dfs -fs $hdfs_url -chown 185:hdfs /user/185 && \
     hdfs dfs -fs $hdfs_url -chmod 700 /user/185"
