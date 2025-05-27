5f124f61d8b39daabf3059f679c2a5940b9792a8
Julien <peloton@lal.in2p3.fr>
2025-05-27 08:45:22 +0200

    SSOFT: restructuration (#987)
    * Bump fink-utils
    
    * Initial ssoft controler
    
    * Update fink_ssoft
    
    * Add options to the SSOFT controller
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Add script to reconstruct SSOFT
    
    * Fix spark args names
    
    * Add main...
    
    * Yearly reconstruction of the SSOFT
    
    * Update resources
    
    * Update dependencies
    
    * Save last ephemerides file with correct vrsion
    
    * Make 4 times more partitions than cores
    
    * Make the script more generic to handle full recomputation and monthly computation
    
    * Enable monthly computation support
    
    * Fix yearly behaviour
    
    * PEP8
    
    * Fail if previous ephemerides does not exist
    
    * PEP8
    
    * Update import with fink-utils==0.36.0
    
    * Fix bug
    
    * Fixed undefined variable
    
    * Remove dot in the version
    
    * Update bin for SSOFT
    
    * Bunp deps
    
    * Documentation
    
    * Bump fink-science
    
    * Remove dot in filename
    
    * Update SSOFT generator
    
    * Fix bugs
    
    * New scheduler for the SSOFT
    
    * Fix space
    
    * Capture output to avoid set -e to trigger
    
    * Better listing
    
    * Release last code
    
    * Remove set -e
    
    * Add version
    
    * Fix type
    
    * Fix type
    
    * Bump fink-science deps
    
    * Add option to transfer the SSOFT
    
    * Enable ROCKS cache
    
    * Fix wrong check
    
    * Add script to compute ephemerides for ATLAS
    
    * Add scheduler for ATLAS
    
    * Add configuration files for ATLAS
    
    * Use ZTF conf file instead of having a conf folder
    
    * Fix wrong name
    
    * Add obscode
    
    * Fix name of argument
    
    * Fix bash syntax
    
    * Try --use-pep517 when installing package
    
    * Script to transfer the SSOFT to HDFS. Beware names are not correct between YYYYMM and YYYY.MM

---
+++ scheduler/atlas/launch_ssoft.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2025 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+source ~/.bash_profile
+
+# re-download latest information
+export ROCKS_CACHE_DIR="no-cache"
+
+for OBSCODE in T05 T08 W68 M22; do
+	fink_ssoft -s atlas -c ${FINK_HOME}/conf/ztf/fink.conf.prod -observer ${OBSCODE} --reconstruct-data > ${FINK_HOME}/broker_logs/atlas_ephemerides_${OBSCODE}.log 2>&1
+done
