b89783582c817f42e4a88feef5d8c33a899a26c3
JulienPeloton <peloton@lal.in2p3.fr>
2025-09-19 15:00:43 +0200

    Rename tables

--- conf/delete_hbase_table.sh
+++ conf/delete_hbase_table.sh
@@ -2,7 +2,8 @@
 
 TABLE_PREFIX="rubin"
 
-STANDARD_TABLES="${TABLE_PREFIX}.diaObject ${TABLE_PREFIX}.diaSource ${TABLE_PREFIX}.cutouts ${TABLE_PREFIX}.forcedSources ${TABLE_PREFIX}.pixel128"
+STANDARD_TABLES="${TABLE_PREFIX}.diaObject ${TABLE_PREFIX}.ssObject ${TABLE_PREFIX}.diaSource_static ${TABLE_PREFIX}.diaSource_sso ${TABLE_PREFIX}.cutouts ${TABLE_PREFIX}.forcedSources ${TABLE_PREFIX}.pixel128"
+
 
 for TABLE_NAME in $STANDARD_TABLES; do
         echo "Processing table $TABLE_NAME"
