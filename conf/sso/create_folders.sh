d41f3c917717ad91b1853a36ce9447859935c914
JulienPeloton <peloton@lal.in2p3.fr>
2025-10-29 17:10:49 +0100

    Add ssocards folder

--- conf/sso/create_folders.sh
+++ conf/sso/create_folders.sh
@@ -2,3 +2,6 @@
 
 mkdir -p /spark_mongo_tmp/julien.peloton
 ln -s /spark-dir/astrodata /spark_mongo_tmp/julien.peloton/astrodata
+
+mkdir -p /spark_mongo_tmp/julien.peloton/ssocards/ssoCard-latest
+ln -s /spark-dir/ssocards/ssoCard-latest /spark_mongo_tmp/julien.peloton/ssocards/ssoCard-latest
