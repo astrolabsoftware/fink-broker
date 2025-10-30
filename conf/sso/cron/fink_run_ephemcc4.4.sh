9557802799d97553ab41e4faab68cc9141aa3dc8
JulienPeloton <peloton@lal.in2p3.fr>
2025-10-30 17:08:31 +0100

    Encode name

--- conf/sso/cron/fink_run_ephemcc4.4.sh
+++ conf/sso/cron/fink_run_ephemcc4.4.sh
@@ -7,7 +7,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/miriade/lib
 
 EPHEMCC4=/opt/miriade/bin
 OTYPE=aster
-NAME=$1
+NAME="$1"
 RPLANE=$2
 TCOOR=$3
 OBSERVER=$4
@@ -17,20 +17,20 @@ IOFILE=$7
 RAMPATH=$8
 UIDD=$9
 
+# encode the name
+ENCODED_NAME="${NAME// /%20}"
+
 # resolve the name
-response=$(curl https://api.ssodnet.imcce.fr/quaero/1/sso/search?q=$NAME)
+response=$(curl https://api.ssodnet.imcce.fr/quaero/1/sso/search?q=${ENCODED_NAME})
 
 # Use jq to extract the id
 id=$(echo "$response" | jq -r '.data[0].id')
-
-# Print the id
-echo "$id"
 
 # For the epehe
 $EPHEMCC4/ephemcc4.4 \
     -u $USERCONF \
     --iofile $IOFILE \
-    -t ssocard:$id \
+    -t ssocard:"$id" \
     -tp $RPLANE \
     -tc $TCOOR \
     -c 3 \
