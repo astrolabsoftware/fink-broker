4ddcc557dab1cf8bef862a21e69fb71e04385944
JulienPeloton <peloton@lal.in2p3.fr>
2025-10-30 10:23:52 +0100

    Fix typo

--- conf/sso/update_astrodata_all2.sh
+++ conf/sso/update_astrodata_all2.sh
@@ -13,19 +13,19 @@ REMOTE_FOLDER="/spark-dir/astrodata"
 CURRENT_DATE=$(date +"%Y%m%d")
 BACKUP_FOLDER="${REMOTE_FOLDER}_${CURRENT_DATE}_backup"
 
-# Step 1: Check if the folder exists on each machine and back it up
+echo "Step 1: Check if the folder exists on each machine and back it up"
 pssh -p 12 -t 100000000 -h "${HOSTFILE}" "if [ -d '${REMOTE_FOLDER}' ]; then
   mv '${REMOTE_FOLDER}' '${BACKUP_FOLDER}' && echo 'Backup created at ${BACKUP_FOLDER} on $(hostname)';
 else
   echo 'No existing folder to backup on $(hostname).';
 fi"
 
-# Step 2: Compress the folder on Machine 1
+echo "Step 2: Compress the folder on master"
 tar -czf astrodata_compressed.tar.gz -C "$(dirname "${FOLDER_TO_COMPRESS}")" "$(basename "${FOLDER_TO_COMPRESS}")"
 
-# Step 3: Transfer the compressed folder to all machines
-pssh.pscp astrodata_compressed.tar.gz -h "${HOSTFILE}:${REMOTE_FOLDER}/"
+echo "Step 3: Transfer the compressed folder to all machines"
+pscp.pssh astrodata_compressed.tar.gz -h "${HOSTFILE}:${REMOTE_FOLDER}/"
 
-# Step 4: Decompress the folder on all machines
+echo "Step 4: Decompress the folder on all machines"
 pssh -h "${HOSTFILE}" "tar -xzf '${REMOTE_FOLDER}/astrodata_compressed.tar.gz' -C '${REMOTE_FOLDER}' && rm '${REMOTE_FOLDER}/astrodata_compressed.tar.gz'"
 
