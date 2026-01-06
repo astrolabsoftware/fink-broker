715023551387f6c1cad1dd5478c74eb40ffb463b
JulienPeloton <peloton@lal.in2p3.fr>
2026-01-06 11:35:47 +0100

    Redesign the SSO resolver

--- bin/rubin/sso_resolver.py
+++ bin/rubin/sso_resolver.py
@@ -26,14 +26,15 @@ from fink_broker.common.spark_utils import list_hdfs_files
 
 
 def main():
-    """Download the TNS catalog, and load it in HBase"""
+    """Table to resolve designation into ssObjectId"""
     parser = argparse.ArgumentParser(description=__doc__)
     args = getargs(parser)
 
     logger = init_logger(args.log_level)
 
-    index_row_key_name = "salt_ssObjectId"
-    columns = index_row_key_name.split("_")
+    # ssObjectId --> designation(s)
+    cols_row_key_name = ["salt", "ssObjectId"]
+    row_key_name = "_".join(cols_row_key_name)
     index_name = ".sso_resolver"
 
     # Initialise Spark session
@@ -57,7 +58,7 @@ def main():
     df = load_parquet_files(paths)
 
     # Keep only SSO
-    df = df.filter(df["ssSource"].isNotNull())
+    df = df.filter(df["mpc_orbits"].isNotNull())
 
     # Add salt
     df = salt_from_last_digits(
@@ -66,7 +67,8 @@ def main():
 
     # Select wanted columns
     cols_rubin = [
-        "mpc_orbits.designation",
+        "mpc_orbits.packed_primary_provisional_designation",
+        "mpc_orbits.unpacked_primary_provisional_designation",
         "ssSource.ssObjectId",
         "diaSource.diaSourceId",
     ]
@@ -74,7 +76,7 @@ def main():
 
     df = df.select(cols_rubin + cols_fink)
 
-    df = add_row_key(df, row_key_name=index_row_key_name, cols=columns)
+    df = add_row_key(df, row_key_name=row_key_name, cols=cols_row_key_name)
 
     # Get rid of salt for HBase
     df = df.drop("salt")
@@ -84,7 +86,7 @@ def main():
     push_to_hbase(
         df=df,
         table_name=args.science_db_name + index_name,
-        rowkeyname=index_row_key_name,
+        rowkeyname=row_key_name,
         cf=cf,
         catfolder=args.science_db_catalogs,
     )
