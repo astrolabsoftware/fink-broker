6b8ed925ba2682912543f8690a415147bdc4a03d
JulienPeloton <peloton@lal.in2p3.fr>
2026-01-16 11:33:20 +0100

    Update eproc

--- conf/sso/install_eproc.sh
+++ conf/sso/install_eproc.sh
@@ -5,11 +5,12 @@ FOLDER=$2
 
 # remove if existing /opt/miriade
 if [ -d "$(readlink -f /opt/miriade)" ]; then
-    rm /opt/miriade
+    rm -r $(readlink -f /opt/miriade)
+    unlink /opt/miriade
 fi
 
 # uncompress
-tar xzvf /opt/$TARNAME -C /
+tar xzf /opt/$TARNAME -C /
 
 # create the symlink
 ln -s $FOLDER /opt/miriade
