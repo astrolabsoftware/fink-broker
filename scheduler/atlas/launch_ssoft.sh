d9a10cdd28c2c1b5ef34e8cabc5959325584175b
JulienPeloton <peloton@lal.in2p3.fr>
2026-01-16 11:34:01 +0100

    Update ATLAS script

--- scheduler/atlas/launch_ssoft.sh
+++ scheduler/atlas/launch_ssoft.sh
@@ -21,5 +21,5 @@ source ~/.bash_profile
 export ROCKS_CACHE_DIR="no-cache"
 
 for OBSCODE in T05 T08 W68 M22; do
-	fink_ssoft -s atlas -c ${FINK_HOME}/conf/ztf/fink.conf.prod -observer ${OBSCODE} --reconstruct-data > ${FINK_HOME}/broker_logs/atlas_ephemerides_${OBSCODE}.log 2>&1
+	fink_ssoft -s atlas -c ${FINK_HOME}/conf/ztf/fink.conf.prod -observer ${OBSCODE} --reconstruct-data > ${FINK_HOME}/broker_logs/atlas_ephemerides_v3_${OBSCODE}.log 2>&1
 done
