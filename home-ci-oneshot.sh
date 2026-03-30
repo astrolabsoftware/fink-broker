28aeb714742b72fa8d4ed5dc83814cb63c095b94
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-03-30 16:04:33 +0200

    Remove useless file

--- home-ci-oneshot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-rm -rf /tmp/fink-ci &&  home-ci run -v 3 -c "$HOME/src/github.com/k8s-school/home-ci/examples/fink-broker-noscience.yaml" -b 1131-home-ci-integration
