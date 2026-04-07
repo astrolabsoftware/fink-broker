c589ba21ab936c1b79bf8d1ba2ed661932ea9167
JulienPeloton <peloton@lal.in2p3.fr>
2026-04-07 08:37:24 +0200

    Ruff and PEP8

--- conf/sso/test/compare_methods.py
+++ conf/sso/test/compare_methods.py
@@ -1,4 +1,5 @@
 """Simply compare local installation of eproc with web service results"""
+
 from fink_utils.sso.miriade import query_miriade_ephemcc
 from fink_utils.sso.miriade import query_miriade
 from astropy.coordinates import SkyCoord
@@ -10,20 +11,17 @@ import pandas as pd
 import numpy as np
 import io
 
-#ident = "10P"
-#ident = "2010 JO69"
-#ident = "C/2020_R2"
-#ident = "302530"
-#ident = "143"
-#ident = "Julienpeloton"
+# ident = "10P"
+# ident = "2010 JO69"
+# ident = "C/2020_R2"
+# ident = "302530"
+# ident = "143"
+# ident = "Julienpeloton"
 ident = "9104_P-L"
 
 r = requests.post(
-    'https://api.ztf.fink-portal.org/api/v1/sso',
-    json={
-        'n_or_d': ident,
-        'output-format': 'json'
-    }
+    "https://api.ztf.fink-portal.org/api/v1/sso",
+    json={"n_or_d": ident, "output-format": "json"},
 )
 
 # Format output in a DataFrame
@@ -32,18 +30,14 @@ pdf = pd.read_json(io.BytesIO(r.content))
 jd = pdf["i:jd"].to_numpy()
 
 parameters = {
-    'outdir': '/tmp/ramdisk/spins',
-    'runner_path': '/tmp/fink_run_ephemcc4.4.sh',
-    'userconf': '/tmp/.eproc-4.4',
-    'iofile': '/tmp/default-ephemcc-observation.xml'
+    "outdir": "/tmp/ramdisk/spins",
+    "runner_path": "/tmp/fink_run_ephemcc4.4.sh",
+    "userconf": "/tmp/.eproc-4.4",
+    "iofile": "/tmp/default-ephemcc-observation.xml",
 }
 
 t0 = time.time()
-ephem_1 = query_miriade_ephemcc(
-    ident,
-    jd,
-    parameters=parameters
-)
+ephem_1 = query_miriade_ephemcc(ident, jd, parameters=parameters)
 print("LOCAL: {:.2f} seconds".format(time.time() - t0))
 
 t0 = time.time()
@@ -57,9 +51,7 @@ for col in common_cols:
     if isinstance(a[0], int) or isinstance(a[0], float):
         print(
             "{}: median deviation = {:.2f}% (max = {:.2f}%)".format(
-                col,
-                np.median(np.abs(1 - a/b)) * 100,
-                np.max(np.abs(1 - a/b)) * 100
+                col, np.median(np.abs(1 - a / b)) * 100, np.max(np.abs(1 - a / b)) * 100
             )
         )
     else:
@@ -69,5 +61,8 @@ for col in common_cols:
 coord_1 = SkyCoord(ephem_1["RA"], ephem_1["DEC"], unit=(u.deg, u.deg))
 coord_2 = SkyCoord(ephem_2["RA"], ephem_2["DEC"], unit=(u.deg, u.deg))
 dev = coord_1.separation(coord_2).deg * 3600
-print("Median position deviation: {:.2f} arcsec (max = {:.2f} arcsec)".format(np.median(dev), np.max(dev)))
-
+print(
+    "Median position deviation: {:.2f} arcsec (max = {:.2f} arcsec)".format(
+        np.median(dev), np.max(dev)
+    )
+)
