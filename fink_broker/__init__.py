20c38078e28d78b1d50796839f4b97b8af99ca6e
JulienPeloton <peloton@lal.in2p3.fr>
2026-04-15 20:47:56 +0200

    Bump to 5.0-rc0

--- fink_broker/__init__.py
+++ fink_broker/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 AstroLab Software
+# Copyright 2019-2026 AstroLab Software
 # Author: Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,4 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "4.1"
+__version__ = "5.0-rc0"
