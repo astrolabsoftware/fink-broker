09d3e1ffe16e414e14e2f70ab76c0a817a0d3048
JulienPeloton <peloton@lal.in2p3.fr>
2026-09-10 15:52:45 +0200

    Refactoring

--- fink_broker/common/ssoft.py
+++ fink_broker/common/ssoft.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 """Construct the Rubin Solar System Object Fink Table (SSOFT)."""
 
+import pyspark.sql.functions as F
+
 import os
 import argparse
 import datetime
@@ -23,7 +25,11 @@ from fink_broker.common.spark_utils import init_sparksession
 
 from fink_science.rubin.ssoft.processor import build_the_ssoft
 from fink_utils.sso.utils import retrieve_last_date_of_previous_month
+from fink_utils.sso.ssoft import join_aggregated_sso_data
 from fink_utils.hdfs.utils import path_exist
+from fink_utils.sso.ssoft import aggregate_rubin_sso_data, aggregate_ztf_sso_data
+from fink_utils.sso.ephem import extract_ephemerides_from_miriade
+from fink_utils.sso.ephem import expand_columns
 
 
 def generate_ssoft(sso_file: str):
@@ -193,3 +199,264 @@ def make_checks(prefix_path, sso_file, year=None, monthly=None, logger=None):
     if not is_data:
         logger.warning("No data found for {}. Skipping...".format(path))
     return is_ephem, is_data
+
+
+def compute_ephemerides(survey: str, sso_file: str):
+    """Compute ephemerides from alert data
+
+    Parameters
+    ----------
+    survey: str
+        ztf or rubin
+    sso_file: str
+        File defined in fink_ssoft -s <ztf|rubin> --link-data
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "-limit",
+        type=int,
+        default=None,
+        help="""
+        Use only `limit` number of SSO per month for test purposes.
+        Default is None, meaning all available data is considered.
+        """,
+    )
+    parser.add_argument(
+        "-prefix_path",
+        type=str,
+        default="archive/science",
+        help="""
+        Prefix path on HDFS to the yearly data.
+        Default is "archive/science".
+        """,
+    )
+    parser.add_argument(
+        "-mode",
+        type=str,
+        default="last_month",
+        help="""
+        Compute last month ephemerides `last_month`, or recompute
+        all ephemerides `all`.
+        """,
+    )
+    args = parser.parse_args(None)
+
+    # Initialise Spark session
+    spark = init_sparksession(
+        name="{}_ephemerides".format(args.mode), shuffle_partitions=100
+    )
+    ncores = int(spark.sparkContext.getConf().get("spark.cores.max"))
+
+    # 4 times more partitions than cores
+    nparts = 4 * ncores
+
+    # The level here should be controlled by an argument.
+    logger = get_fink_logger(spark.sparkContext.appName, "INFO")
+
+    # debug statements
+    inspect_application(logger)
+
+    if args.mode == "all":
+        years = range(2019, datetime.datetime.now().year + 1)
+        logger.info("Processing data from {} to {}".format(years[0], years[-1]))
+
+        if survey == "rubin":
+            df_new = aggregate_and_add_ephem_rubin(
+                None, None, nparts, args.prefix_path, args.limit, logger
+            )
+        elif survey == "ztf":
+            df_new = aggregate_and_add_ephem_ztf(
+                None, None, nparts, args.prefix_path, args.limit, logger
+            )
+
+        df_new.write.mode("overwrite").parquet(sso_file.format(years[-1], ""))
+    elif args.mode == "last_month":
+        # get last month coordinates
+        lm = retrieve_last_date_of_previous_month(datetime.datetime.now())
+
+        # make checks
+        is_ephem, is_data = make_checks(
+            args.prefix_path, sso_file, monthly=True, logger=logger
+        )
+        if not is_ephem and is_data:
+            # make computation
+            if survey == "rubin":
+                df_new = aggregate_and_add_ephem_rubin(
+                    lm.year,
+                    "{:02d}".format(lm.month),
+                    nparts,
+                    args.prefix_path,
+                    args.limit,
+                    logger,
+                )
+            elif survey == "ztf":
+                df_new = aggregate_and_add_ephem_ztf(
+                    lm.year,
+                    "{:02d}".format(lm.month),
+                    nparts,
+                    args.prefix_path,
+                    args.limit,
+                    logger,
+                )
+
+            logger.info("Loading previous ephemerides data...")
+            prev_ephem_folder = sso_file.format(lm.year, "{:02d}".format(lm.month))
+            if not path_exist(prev_ephem_folder):
+                logger.warning(
+                    "{} does not exist. Exiting...".format(prev_ephem_folder)
+                )
+                return 1
+
+            df_prev = spark.read.format("parquet").load(
+                sso_file.format(lm.year, "{:02d}".format(lm.month))
+            )
+
+            logger.info("Joining previous and new data...")
+            assert sorted(df_prev.columns) == sorted(df_new.columns), (
+                df_prev.columns,
+                df_new.columns,
+            )
+            df_join = join_aggregated_sso_data(df_prev, df_new, on="ssnamenr")
+
+            curr = datetime.datetime.now()
+            current_month = "{:02d}".format(curr.month)
+            df_join.write.mode("overwrite").parquet(
+                sso_file.format(curr.year, current_month)
+            )
+        else:
+            return 1
+
+
+def aggregate_and_add_ephem_rubin(year, month, npart, prefix_path, limit, logger):
+    """Wrapper to get new Rubin data and ephemerides
+
+    Notes
+    -----
+    We might need to enable weekly aggregation.
+
+    Parameters
+    ----------
+    year: str
+        Year in format YYYY
+    month: str
+        Month in format MM. Leading zero must be there.
+        For yearly aggregation, set month to None.
+    npart: int
+        Number of Spark partitions. Rule of thumb: 4 times
+        the number of cores.
+    prefix_path: str
+        Prefix path to Fink/Rubin data
+    limit: int
+        If set, limit the number of object to process.
+        Otherwise, put to None.
+
+
+    Returns
+    -------
+    out: Spark DataFrame
+    """
+    if (year is None) and (month is None):
+        logger.info("Aggregating ALL data")
+        df_new = aggregate_rubin_sso_data(prefix_path=prefix_path)
+    elif (year is not None) and (month is None):
+        logger.info("Aggregating data from {}".format(year))
+        current_year = datetime.datetime.now().year
+        df_new = aggregate_rubin_sso_data(
+            year=year,
+            stop_previous_month=(year == current_year),
+            prefix_path=prefix_path,
+        )
+    elif (year is not None) and (month is not None):
+        logger.info("Aggregating data from {}{}".format(year, month))
+        df_new = aggregate_rubin_sso_data(
+            year=year, month=month, prefix_path=prefix_path
+        )
+
+    if limit is not None:
+        assert isinstance(limit, int), (limit, type(limit))
+        logger.info("Limiting the new number of objects to {}".format(limit))
+        df_new = df_new.limit(limit)
+
+    df_new = df_new.repartition(npart).cache()
+    logger.info("{} objects".format(df_new.count()))
+
+    col_ = "ephem"
+    df_new = df_new.withColumn(
+        col_,
+        extract_ephemerides_from_miriade(
+            "designation",
+            "cjdUtc",
+            F.lit("X05"),
+            F.lit(0.0),
+            F.expr("uuid()"),
+            F.lit("ephemcc"),
+            F.lit("ephemcc-photom.xml"),
+        ),
+    )
+    df_expanded = expand_columns(df_new, col_to_expand=col_)
+    return df_expanded
+
+
+def aggregate_and_add_ephem_ztf(year, month, npart, prefix_path, limit, logger):
+    """Wrapper to get new ZTF data and ephemerides
+
+    Parameters
+    ----------
+    year: str
+        Year in format YYYY
+    month: str
+        Month in format MM. Leading zero must be there.
+        For yearly aggregation, set month to None.
+    npart: int
+        Number of Spark partitions. Rule of thumb: 4 times
+        the number of cores.
+    prefix_path: str
+        Prefix path to Fink/ZTF data
+    limit: int
+        If set, limit the number of object to process.
+        Otherwise, put to None.
+
+
+    Returns
+    -------
+    out: Spark DataFrame
+    """
+    if (year is None) and (month is None):
+        logger.info("Aggregating ALL data")
+        df_new = aggregate_ztf_sso_data(prefix_path=prefix_path)
+    elif (year is not None) and (month is None):
+        logger.info("Aggregating data from {}".format(year))
+        current_year = datetime.datetime.now().year
+        df_new = aggregate_ztf_sso_data(
+            year=year,
+            stop_previous_month=(year == current_year),
+            prefix_path=prefix_path,
+        )
+    elif (year is not None) and (month is not None):
+        logger.info("Aggregating data from {}{}".format(year, month))
+        df_new = aggregate_ztf_sso_data(year=year, month=month, prefix_path=prefix_path)
+
+    if limit is not None:
+        assert isinstance(limit, int), (limit, type(limit))
+        logger.info("Limiting the new number of objects to {}".format(limit))
+        df_new = df_new.limit(limit)
+
+    df_new = df_new.repartition(npart).cache()
+    logger.info("{} objects".format(df_new.count()))
+
+    col_ = "ephem"
+    df_new = df_new.withColumn(
+        col_,
+        extract_ephemerides_from_miriade(
+            "ssnamenr",
+            "cjd",
+            F.lit("I41"),
+            F.lit(15.0),
+            F.expr("uuid()"),
+            F.lit("ephemcc"),
+            F.lit("ephemcc-photom.xml"),
+        ),
+    )
+    df_expanded = expand_columns(df_new, col_to_expand=col_)
+    return df_expanded
