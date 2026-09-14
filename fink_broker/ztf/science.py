4908cd3ef388d9f19391a59aa5480c9c2cff6a57
JulienPeloton <peloton@lal.in2p3.fr>
2026-09-14 12:06:50 +0200

    Increase REGALADE radius from 1.2 to 20 acrseconds

--- fink_broker/ztf/science.py
+++ fink_broker/ztf/science.py
@@ -153,10 +153,10 @@ def apply_all_xmatch(df, tns_raw_output):
         ),
     )
 
-    _LOG.info("New processor: REGALADE (1.2 arcsec)")
+    _LOG.info("New processor: REGALADE (20 arcsec)")
     df = xmatch_cds(
         df,
-        distmaxarcsec=1.2,
+        distmaxarcsec=20.,
         catalogname="vizier:J/A+A/706/A284/regalade",
         cols_out=["R1", "R2", "PA", "z", "ezin", "RAJ2000", "DEJ2000"],
         types=["float", "float", "float", "float", "float", "float", "float"],
